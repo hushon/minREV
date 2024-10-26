c019a589f828229975840e397e2aaa31d5a08e6f
Hyounguk Shon <hyounguk.shon@gmail.com>
2024-10-26 16:58:10 +0000

    updated

--- rev_custom.py
+++ rev_custom.py
@@ -334,6 +334,94 @@ class CouplingBlockRef(nn.Module):
         return output
 
 
+class InvertibleTransformerBlock(CouplingBlock):
+    def __init__(self, dim, num_heads):
+        super().__init__(
+            F = AttentionSubBlock(dim=dim, num_heads=num_heads),
+            G = MLPSubblock(dim=dim)
+        )
+
+
+class InvertibleVisionTransformer(nn.Module):
+    def __init__(
+        self,
+        embed_dim=768,
+        n_head=8,
+        depth=8,
+        patch_size=(2, 2),  # this patch size is used for CIFAR-10 --> (32 // 2)**2 = 256 sequence length
+        image_size=(32, 32),  # CIFAR-10 image size
+        num_classes=10
+    ):
+        super().__init__()
+
+        self.embed_dim = embed_dim
+        self.n_head = n_head
+        self.depth = depth
+        self.patch_size = patch_size
+
+        num_patches = (image_size[0] // self.patch_size[0]) * \
+            (image_size[1] // self.patch_size[1])
+
+        self.layers = nn.ModuleList(
+            [
+                InvertibleTransformerBlock(
+                    dim=self.embed_dim,
+                    num_heads=self.n_head
+                )
+                for _ in range(self.depth)
+            ]
+        )
+
+        # Standard Patchification and absolute positional embeddings as in ViT
+        self.patch_embed = nn.Conv2d(
+            3, self.embed_dim, kernel_size=patch_size, stride=patch_size
+        )
+
+        self.pos_embeddings = nn.Parameter(
+            torch.zeros(1, num_patches, self.embed_dim)
+        )
+
+        # The two streams are concatenated and passed through a linear
+        # layer for final projection. This is the only part of RevViT
+        # that uses different parameters/FLOPs than a standard ViT model.
+        # Note that fusion can be done in several ways, including
+        # more expressive methods like in paper, but we use
+        # linear layer + LN for simplicity.
+        self.head = nn.Linear(2 * self.embed_dim, num_classes, bias=True)
+        self.norm = nn.LayerNorm(2 * self.embed_dim)
+
+    def forward(self, x):
+        x = self.patch_embed(x).flatten(2).transpose(1, 2)
+        x += self.pos_embeddings
+
+        # the two streams X_1 and X_2 are initialized identically with x and
+        # concatenated along the last dimension to pass into the reversible blocks
+        x = torch.cat([x, x], dim=-1)
+
+        # no need for custom backprop in eval/inference phase
+        if not self.training or self.no_custom_backward:
+            executing_fn = RevViT.vanilla_backward
+        else:
+            executing_fn = RevBackProp.apply
+
+        # This takes care of switching between vanilla backprop and rev backprop
+        for layer in self.layers:
+            x, ctx = layer(x, ctx)
+        ctx.output = x.detach()
+
+        # aggregate across sequence length
+        x = x.mean(1)
+
+        # head pre-norm
+        x = self.norm(x)
+
+        # pre-softmax logits
+        x = self.head(x)
+
+        # return pre-softmax logits
+        return x
+
+
 class ReversibleBlock(nn.Module):
     """
     Reversible Blocks for Reversible Vision Transformer.
